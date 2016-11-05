cbb513d647288d97a91785b476b946bf3ba5ce22
richiepixel <Richie@MacBook-Pro-de-Richie.local>
2016-11-05 11:38:16 -0600

    Add support to listen by /mail/process path

--- MyPersonalPage/createNodeServer.js
+++ MyPersonalPage/createNodeServer.js
@@ -6,6 +6,9 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
+  if(req.url === '/mail/process'){
+  	res.end('Mail process path\n');
+  }
   res.end('Hola Mundo\n');
 });
 
